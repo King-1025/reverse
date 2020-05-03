e67f6be55270ff539a0228ceeeb0c16b95a381ff
King-1025 <1543641386@qq.com>
2020-05-04 01:12:43 +0800

    fix

---
+++ script/anss.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+ROOT=$(pwd)
+RESULT=$TASK_RESULT
+ANSS=$ROOT/anss
+
+echo ROOT: $ROOT
+echo RESULT: $RESULT
+
+export ESC_LOCAL_PATH=$ANSS
+
+mkdir -p $ANSS $RESULT && \
+esc_ng download 11352974 && \
+tar -xzf $ANSS/anss.tar.gz -C $ANSS && \
+apktool b -o $RESULT/anss.apk -k -m --use-aapt2 $ANSS/anss
+
+if [ -e $RESULT ]; then
+   esc_ng upload $RESULT
+fi
+
+rm -rf $ANSS
